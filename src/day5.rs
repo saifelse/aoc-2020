0c89be0d006ef59965a51dc1aa34846741a86a4e
Saif Hakim <saif@benchling.com>
2020-12-10 07:11:32 -0800

    clean up day5

--- src/day5.rs
+++ src/day5.rs
@@ -1,28 +1,30 @@
-use std::collections::HashSet;
+use itertools::sorted;
 use std::clone::Clone;
+use std::collections::HashSet;
 use std::iter::Iterator;
 
 trait CloneIterator: Iterator + Clone {}
-
 
 pub fn seat_to_id(seat: &str) -> i32 {
     // This is just a binary encoding described in a very roundabout way...
     // BFFFBBFRRR --> 0b1000110111 --> 567
     // FFFBBBFRRR --> 0b0001110111 --> 119
     // BBFFBBFRLL --> 0b1100110100 --> 820
-    seat.chars().fold(0, |acc, b| acc * 2 + match b {
-        'F' => 0,
-        'B' => 1,
-        'L' => 0,
-        'R' => 1,
-        _ => panic!(),
+    seat.chars().fold(0, |acc, b| {
+        acc * 2
+            + match b {
+                'F' => 0,
+                'B' => 1,
+                'L' => 0,
+                'R' => 1,
+                _ => panic!(),
+            }
     })
 }
 
 #[aoc(day5, part1)]
 pub fn solve_part1(input: &str) -> i32 {
     input.lines().map(|l| seat_to_id(l)).max().unwrap()
-
 }
 
 // This doesn't work because types and memory management are hard.
@@ -33,7 +35,7 @@ pub fn solve_part1(input: &str) -> i32 {
 //     const width: usize = 10;
 //     let rows = (data.len() + 1) / (width + 1);
 //     let it: Box<dyn CloneIterator<Item = i32>> = Box::new((0..(rows+1)));
-//     for j in 0..width { 
+//     for j in 0..width {
 //         let mut maybe_next_iter = it.clone().filter(|i| matches!(data[i * (width + 1) + j], b'B' | b'R')).peekable();
 //         if maybe_next_iter.peek().is_some() {
 //             it = &maybe_next_iter;
@@ -41,7 +43,6 @@ pub fn solve_part1(input: &str) -> i32 {
 //     }
 //     it.next().unwrap()
 // }
-
 
 #[aoc(day5, part2)]
 pub fn solve_part2(input: &str) -> i32 {
@@ -51,7 +52,7 @@ pub fn solve_part2(input: &str) -> i32 {
     while let Some(seat_id) = iter.next() {
         if let Some(next_seat_id) = iter.peek() {
             if seat_id + 2 == **next_seat_id {
-                return seat_id + 1
+                return seat_id + 1;
             }
         }
     }
@@ -62,6 +63,18 @@ pub fn solve_part2(input: &str) -> i32 {
 #[aoc(day5, part2, hashset)]
 pub fn solve_part2_hashset(input: &str) -> i32 {
     let taken: HashSet<i32> = input.lines().map(|l| seat_to_id(l)).collect();
-    (0..1024).find(|x| taken.contains(&(x - 1)) && !taken.contains(&x) && taken.contains(&(x + 1))).unwrap()
+    (0..1024)
+        .find(|x| taken.contains(&(x - 1)) && !taken.contains(&x) && taken.contains(&(x + 1)))
+        .unwrap()
 }
 
+// Basically as fast as solve_part2
+#[aoc(day5, part2, window)]
+pub fn solve_part2_window(input: &str) -> i32 {
+    sorted(input.lines().map(|l| seat_to_id(l)))
+        .collect::<Vec<i32>>()
+        .windows(2)
+        .find(|w| w[1] - w[0] == 2)
+        .unwrap()[0]
+        + 1
+}
