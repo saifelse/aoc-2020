11f45005de01f5d6fa296df14dc10a39bd278585
Saif Hakim <saif@benchling.com>
2020-12-14 01:28:52 -0800

    day3 clenaup

--- src/day3.rs
+++ src/day3.rs
@@ -1,50 +1,46 @@
-#[aoc(day3, part1)]
-pub fn solve_part1(input: &str) -> i32 {
-    let width = input.find('\n').unwrap();
-    let map = input.as_bytes();
-    let height = (map.len() + 1) / (width + 1);
-    let tree = "#".as_bytes().iter().next().unwrap();
-    let mut i: usize= 0;
-    let mut j: usize = 0;
-    let mut count = 0;
-    while i < height - 1 {
-        j = (j + 3) % width;
-        i = i + 1;
-        let byte = map[i * (width + 1) + j];
-        if byte == *tree {
-            count += 1;
-        }
-    }
-    return count;
+const TREE: u8 = b'#';
+
+struct Map<'a> {
+    map: &'a [u8],
+    width: usize,
+    height: usize,
 }
 
-fn traverse(dj: usize, di: usize, width: usize, height: usize, map: &[u8], obstacle: &u8) -> u32 {
-    let mut i: usize= 0;
-    let mut j: usize = 0;
-    let mut count = 0;
-    while i < height - di {
-        j = (j + dj) % width;
-        i = i + di;
-        let byte = map[i * (width + 1) + j];
-        if byte == *obstacle {
-            count += 1;
-        }
+impl<'a> Map<'a> {
+    fn parse(input: &str) -> Map {
+        let map = input.as_bytes();
+        let width = input.find('\n').unwrap();
+        let height = (map.len() + 1) / (width + 1);
+        Map { map, width, height }
     }
-    return count;
+    fn traverse(&self, dj: usize, di: usize) -> i64 {
+        let mut i: usize = 0;
+        let mut j: usize = 0;
+        let mut count = 0;
+        while i < self.height - di {
+            j = (j + dj) % self.width;
+            i = i + di;
+            let byte = self.map[i * (self.width + 1) + j];
+            if byte == TREE {
+                count += 1;
+            }
+        }
+        return count;
+    }
 }
+
+#[aoc(day3, part1)]
+pub fn solve_part1(input: &str) -> i64 {
+    let map = Map::parse(input);
+    map.traverse(3, 1)
+}
+
 
 #[aoc(day3, part2)]
-pub fn solve_part2(input: &str) -> u32 {
-    let width = input.find('\n').unwrap();
-    let map = input.as_bytes();
-    let height = (map.len() + 1) / (width + 1);
-    let tree = "#".as_bytes().iter().next().unwrap();
-    return 
-        1
-        * traverse(1, 1, width, height, &map, tree)
-        * traverse(3, 1, width, height, &map, tree)
-        * traverse(5, 1, width, height, &map, tree)
-        * traverse(7, 1, width, height, &map, tree)
-        * traverse(1, 2, width, height, &map, tree);
+pub fn solve_part2(input: &str) -> i64 {
+    let map = Map::parse(input);
+    ([(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)] as [(usize, usize); 5])
+        .iter()
+        .map(|(dj, di)| map.traverse(*dj, *di))
+        .product()
 }
-
